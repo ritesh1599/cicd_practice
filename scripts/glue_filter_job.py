37a921973153bdbb710d4a4a9f02554b2a361c9e
ritesh1599 <ritesh.fupta.1599@gmail.com>
2025-11-12 09:49:41 +0530

    did correction related to path error

--- scripts/glue_filter_job.py
+++ scripts/glue_filter_job.py
@@ -1,5 +1,6 @@
 # scripts/glue_filter_job.py
-import sys
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import argparse
 import os
 from etl.transform import filter_records
